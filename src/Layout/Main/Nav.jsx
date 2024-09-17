01f8312c5e4ba774efdde4eef4a5fce3e4b11d60
hasanonik <jh8539615@gmail.com>
2024-09-18 00:37:11 +0600

    navbar update

--- src/Layout/Main/Nav.jsx
+++ src/Layout/Main/Nav.jsx
@@ -7,7 +7,7 @@ export function Nav() {
   return (
     <Navbar fluid rounded>
       <Navbar.Brand href="https://flowbite-react.com">
-        
+
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">ProLance</span>
       </Navbar.Brand>
       <div className="flex md:order-2">
@@ -25,6 +25,8 @@ export function Nav() {
           <Dropdown.Item>Dashboard</Dropdown.Item>
           <Dropdown.Item>Settings</Dropdown.Item>
           <Dropdown.Item>Earnings</Dropdown.Item>
+          <Dropdown.Item>Earnings</Dropdown.Item>
+          <Dropdown.Item>Earnings</Dropdown.Item>
           <Dropdown.Divider />
           <Dropdown.Item>Sign out</Dropdown.Item>
         </Dropdown>
@@ -37,6 +39,8 @@ export function Nav() {
         <Navbar.Link href="#">About</Navbar.Link>
         <Navbar.Link href="#">Services</Navbar.Link>
         <Navbar.Link href="#">Pricing</Navbar.Link>
+        <Navbar.Link href="#">Contact</Navbar.Link>
+        <Navbar.Link href="#">Contact</Navbar.Link>
         <Navbar.Link href="#">Contact</Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
